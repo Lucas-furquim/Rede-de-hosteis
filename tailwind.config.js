7c533cfc544e8aa9bf99fffd02d80a267a8a9d50
Furquim~ <lucasviniu897@gmail.com>
2024-05-24 18:57:43 -0300

    mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,31 @@ module.exports = {
         cem2: "42%",
         cem3: "54%",
       },
+      screens: {
+        mxl: { max: "1200px" },
+        // => @media (max-width: 1279px) { ... }
+
+        mlg: { max: "1023px" },
+        // => @media (max-width: 1023px) { ... }
+
+        mtrab: { max: "850px" },
+        // => @media (max-width: 850pxpx) { para a pagina de trabalho }
+
+        trab: { max: "900px" },
+        // => @media (max-width: 900pxpx) { a sessão de trabalho na home }
+
+        mmd: { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+
+        msm: { max: "639px" },
+        // => @media (max-width: 639px) { ... }
+
+        mimg: { max: "550px" },
+
+        mximg: { max: "490px" },
+
+        mxs: { max: "439px" },
+      },
     },
   },
   plugins: [require("prettier-plugin-tailwindcss")],
